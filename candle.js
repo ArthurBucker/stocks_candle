04531521377ad81ad3fa943ce581937d364f0f56
Arthur Bucker <arthurbucker@gmail.com>
2021-06-01 03:46:18 -0300

    Melhoramentos na interface

--- candle.js
+++ candle.js
@@ -6,15 +6,27 @@ const txt = document.querySelector('.pesquisar-txt'),
       grafico = document.querySelector('#chart'),
       txtErro = document.querySelector('.erro'),
       txtResult = document.querySelector('.best-results'),
+      nomeAcao = document.querySelector('.nome-acao'),
       button = document.querySelector('.pesquisar');
       
 //Erros
 function erro(res) {
   if(txt.value === '') {
-    throw new Error(txtErro.innerHTML = 'Favor digitar código da ação desejada');
+    throw new Error(
+      txtErro.style.display = 'block',
+      txtErro.innerHTML = 'Favor digitar código da ação desejada',
+      setTimeout(() => {
+        txtErro.style.display = 'none';
+      }, 3000)
+      );
   }
   if(!res.ok) {
-    throw  new Error(txtErro.innerHTML =`${res.statusText}`);
+    throw  new Error(txtErro.style.display = 'block',
+    txtErro.innerHTML = 'SINTO MUITO! Código inválido ou limite de pesquisa atingido',
+    setTimeout(() => {
+      txtErro.style.display = 'none';
+    }, 3000)
+    );
   }
   return res
 }
@@ -23,40 +35,45 @@ function erro(res) {
 let globalTimeout = null;
 
 txt.addEventListener('keyup', (e) => {
-  txtResult.innerHTML = '';
-  if(globalTimeout !== null) {
-    clearTimeout(globalTimeout)
-  } 
-  globalTimeout = setTimeout(searchFunc,500)
+  if (e.keyCode !== 13) {
+    txtResult.innerHTML = '';
+    if(globalTimeout !== null) {
+      clearTimeout(globalTimeout)
+    } 
+    globalTimeout = setTimeout(searchFunc,500)
 
-  function searchFunc(){
-    globalTimeout = null;
-    stocks.getCod(e.target.value)
-    .then(dados => {
-      const buscador = dados.cod['bestMatches'];
-      
-      let objetos = [];
-      let stk = [];
-      let posicao = 0;
-      for (key in buscador) {
-        objetos.push(buscador[key])
-      }
-      
-      for (key in buscador) {
-        stk.push(objetos[posicao]['1. symbol']);
-        txtResult.innerHTML += `<p class="resultados">${stk[posicao]}</p>`;
-        posicao++
-      }
-      console.log(posicao)
-      
-  
-    })
-    .catch(erro)
-     
-    txt.style.borderBottom = 'none';
-    txtResult.style.display = 'block';
-
+    function searchFunc(){
+      globalTimeout = null;
+      stocks.getCod(e.target.value)
+      .then(dados => {
+        const buscador = dados.cod['bestMatches'];
+        
+        let objetos = [];
+        let stk = [];
+        let posicao = 0;
+        for (key in buscador) {
+          objetos.push(buscador[key])
+        }
+        
+        for (key in buscador) {
+          stk.push(objetos[posicao]['1. symbol']);
+          txtResult.innerHTML += `<p class="resultados">${stk[posicao]}</p>`;
+          posicao++
+        }
+        
     
+      })
+      .catch(erro)
+      
+      txt.style.borderBottom = 'none';
+      txtResult.style.display = 'block';
+    }
+  }
+  
+  if(e.keyCode === 13) {
+    setTimeout(() => {
+      txtResult.innerHTML = '';
+    }, 900);
     
   }
     
@@ -65,7 +82,6 @@ txt.addEventListener('keyup', (e) => {
 txtResult.addEventListener('click', (e) => {
   txt.value = e.target.textContent;
   txtResult.style.display = 'none';
-  txt.style.borderBottom = 'solid 2px #ddd';
   e.preventDefault()
 })
 
@@ -78,8 +94,10 @@ button.addEventListener('click', (e) => {
   grafico.innerHTML ='';
   stocks.getDia(txt.value)
   .then(dados => {
+    const nome = dados.dia['Meta Data']['2. Symbol'];
+    const nomeU = nome.toUpperCase();
     const stocksTempo = dados.dia['Time Series (Daily)'];
-    
+    nomeAcao.textContent = nome;
     let dias = [];
     let medidas = [];
     let open = [];
@@ -119,16 +137,36 @@ button.addEventListener('click', (e) => {
       height: 350
     },
     title: {
-      text: 'CandleStick Chart',
-      align: 'left'
+      text: nomeU,
+      align: 'left',
+      style: {
+        fontSize: '20px',
+        color: 'rgb(59, 144, 200)',
+        background: '#00E396'
+      },
     },
     xaxis: {
-      type: 'datetime'
+      type: 'datetime',
+      labels: {
+        style: {
+          fontSize: '12px',
+          colors: '#fff',
+          background: '#00E396'
+        },
+      },
+      
     },
     yaxis: {
       tooltip: {
         enabled: true
-      }
+      },
+      labels: {
+        style: {
+          fontSize: '12px',
+          colors: '#fff',
+          background: '#00E396'
+        },
+      },
     }
     };
  
@@ -140,6 +178,18 @@ button.addEventListener('click', (e) => {
   )
   
   .catch(erro);
+  setTimeout(() => {
+    txt.value = '';
+    grafico.style.background = '#333';
+    txtResult.innerHTML = '';
+    if (grafico.innerHTML !== '') {
+      select.style.display = 'block';
+      select.style.opacity = '1';
+    }
+    
+  },900)
+
+  
   e.preventDefault()
 })
 
@@ -160,8 +210,10 @@ select.addEventListener('change', () => {
   
     //Semana
     case 'TIME_SERIES_WEEKLY':
-        stocks.getSemana(txt.value,tempo)
+    stocks.getSemana(nomeAcao.textContent,tempo)
     .then(dados => {
+      const nome = dados.semana['Meta Data']['2. Symbol'];
+      const nomeU = nome.toUpperCase();
       const stocksTempo = dados.semana['Weekly Time Series'];
       
       let dias = [];
@@ -202,16 +254,36 @@ select.addEventListener('change', () => {
         height: 350
       },
       title: {
-        text: 'CandleStick Chart',
-        align: 'left'
+        text: nomeU,
+        align: 'left',
+        style: {
+          fontSize: '20px',
+          color: 'rgb(59, 144, 200)',
+          background: '#00E396'
+        },
       },
       xaxis: {
-        type: 'datetime'
+        type: 'datetime',
+        labels: {
+          style: {
+            fontSize: '12px',
+            colors: '#fff',
+            background: '#00E396'
+          },
+        },
+        
       },
       yaxis: {
         tooltip: {
           enabled: true
-        }
+        },
+        labels: {
+          style: {
+            fontSize: '12px',
+            colors: '#fff',
+            background: '#00E396'
+          },
+        },
       }
       };
   
@@ -226,73 +298,97 @@ select.addEventListener('change', () => {
 
   //Mês
   case 'TIME_SERIES_MONTHLY':
-    stocks.getMes(txt.value,tempo)
-.then(dados => {
-  const stocksTempo = dados.mes['Monthly Time Series'];
-  
-  let dias = [];
-  let medidas = [];
-  let open = [];
-  let high = [];
-  let low = [];
-  let close = [];
-  let posicao = 0
+    stocks.getMes(nomeAcao.textContent,tempo)
+    .then(dados => {
+      const nome = dados.mes['Meta Data']['2. Symbol'];
+      const nomeU = nome.toUpperCase();
+      const stocksTempo = dados.mes['Monthly Time Series'];
+      
+      let dias = [];
+      let medidas = [];
+      let open = [];
+      let high = [];
+      let low = [];
+      let close = [];
+      let posicao = 0
 
-  for (key in stocksTempo) {
-    dias.push(key);
-  }
-  for (key in stocksTempo[dias[0]]) {
-    medidas.push(key)
-  }
-  for (key in stocksTempo) {
-    open.push(stocksTempo[key][medidas[0]]);
-    high.push(stocksTempo[key][medidas[1]]);
-    low.push(stocksTempo[key][medidas[2]]);
-    close.push(stocksTempo[key][medidas[3]]);
-  }
-  let data = []
- 
-  for (key in stocksTempo) {
-    data[posicao] = ({
-      x: key,
-      y: [open[posicao], high[posicao], low[posicao], close[posicao]]
-    })
-    posicao++
-  }
-  var options = {
-    series: [{
-    data
-  }],
-  chart: {
-    type: 'candlestick',
-    height: 350
-  },
-  title: {
-    text: 'CandleStick Chart',
-    align: 'left'
-  },
-  xaxis: {
-    type: 'datetime'
-  },
-  yaxis: {
-    tooltip: {
-      enabled: true
+      for (key in stocksTempo) {
+        dias.push(key);
+      }
+      for (key in stocksTempo[dias[0]]) {
+        medidas.push(key)
+      }
+      for (key in stocksTempo) {
+        open.push(stocksTempo[key][medidas[0]]);
+        high.push(stocksTempo[key][medidas[1]]);
+        low.push(stocksTempo[key][medidas[2]]);
+        close.push(stocksTempo[key][medidas[3]]);
+      }
+      let data = []
+    
+      for (key in stocksTempo) {
+        data[posicao] = ({
+          x: key,
+          y: [open[posicao], high[posicao], low[posicao], close[posicao]]
+        })
+        posicao++
+      }
+      var options = {
+        series: [{
+        data
+      }],
+      chart: {
+        type: 'candlestick',
+        height: 350,
+      },
+      title: {
+        text: nomeU,
+        align: 'left',
+        style: {
+          fontSize: '20px',
+          color: 'rgb(59, 144, 200)',
+          background: '#00E396'
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+        labels: {
+          style: {
+            fontSize: '12px',
+            colors: '#fff',
+            background: '#00E396'
+          },
+        },
+        
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true
+        },
+        labels: {
+          style: {
+            fontSize: '12px',
+            colors: '#fff',
+            background: '#00E396'
+          },
+        },
+      }
+      };
+
+      var chart = new ApexCharts(document.querySelector("#chart"), options);
+      chart.render();
     }
-  }
-  };
 
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
-}
-
-)
-.catch(erro);;
-break;
+    )
+    .catch(erro);;
+    break;
 
 default:
 
- stocks.getDia(txt.value)
+ stocks.getDia(nomeAcao.textContent)
   .then(dados => {
+    const nome = dados.dia['Meta Data']['2. Symbol'];
+    const nomeU = nome.toUpperCase()
     const stocksTempo = dados.dia['Time Series (Daily)'];
     
     let dias = [];
@@ -324,7 +420,6 @@ default:
       })
       posicao++
     }
-    console.log(posicao)
     var options = {
       series: [{
       data
@@ -334,16 +429,36 @@ default:
       height: 350
     },
     title: {
-      text: 'CandleStick Chart',
-      align: 'left'
+      text: nomeU,
+      align: 'left',
+      style: {
+        fontSize: '20px',
+        color: 'rgb(59, 144, 200)',
+        background: '#00E396'
+      },
     },
     xaxis: {
-      type: 'datetime'
+      type: 'datetime',
+      labels: {
+        style: {
+          fontSize: '12px',
+          colors: '#fff',
+          background: '#00E396'
+        },
+      },
+      
     },
     yaxis: {
       tooltip: {
         enabled: true
-      }
+      },
+      labels: {
+        style: {
+          fontSize: '12px',
+          colors: '#fff',
+          background: '#00E396'
+        },
+      },
     }
     };
  
@@ -357,4 +472,5 @@ default:
 
 
 })
+
 
